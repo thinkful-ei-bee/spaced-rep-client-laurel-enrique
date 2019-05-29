2d0fb1c57fdb9e0e6ffb604d2ecc15f15e6aadd1
LaurelButler <laurelbutler126@gmail.com>
2019-05-29 16:32:35 -0400

    styling for little index cards

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -4,7 +4,7 @@ export default function Word(props){
 
 
     return(
-        <div>
+        <div className="word-card">
             <h4>{props.word.original}</h4>        
             <p>correct answer count: {props.word.correct_count}</p>
             <p>incorrect answer count: {props.word.incorrect_count}</p>        
