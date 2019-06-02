d061ed4f037600ee0b501dec849c76570debab0e
Enrique Montemayor <eemontemayor@live.com>
2019-06-02 17:59:01 -0500

    all test passing

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import LangContext from '../../contexts/LangContext'
 import LangService from '../../services/lang-service'
+import Button from '../../components/Button/Button'
+
+
+
 class LearningRoute extends Component {
   constructor(){
     super();
@@ -18,8 +22,9 @@ class LearningRoute extends Component {
           [key]:value,
         })
       }
-      // console.log(this.state)
+     
     })
+    console.log(this.state)
   }
 
 
@@ -28,31 +33,39 @@ class LearningRoute extends Component {
     this.setState({
       [e.target.name]: e.target.value
     });
-    // console.log(this.state)
   }
 
 
   handleSubmit = (e) =>{
     e.preventDefault()
-  console.log(this.state);
+ 
   //keeps track of word user just finished submitting
   this.setState({preWord: this.state.nextWord});
-    LangService.postWord(this.state.guess_input)
-    .then(res => {
-      // console.log(res)
-      for(const [key,value] of Object.entries(res)){
-        this.setState({
-          [key]:value,
-        })
-      }
-     })
-     
+
+  LangService.postWord(this.state.guess_input)
+  .then(res => {
+    for(const [key,value] of Object.entries(res)){
+      this.setState({
+        [key]:value,
+      })
+    }
+  })
   }
+
+
+  handleNext=(e)=>{
+    e.preventDefault()
+    console.log('here')
+    console.log(this.state)
+    this.setState({...this.state, answer:null})
+  }
+
+
 
   render() {
     // console.log(this.context);
     // console.log(this.props);
-    console.log(this.state);
+    // console.log(this.state);
 
     //when there is an answer
     let message = null;
@@ -61,19 +74,19 @@ class LearningRoute extends Component {
 
     if(this.state.isCorrect) {
       message = <><h2>You were correct! :D</h2></>;
-      displayForm = <><button type='submit'>Try another word!</button></>
+      displayForm = <><button type='click' onClick={this.handleNext.bind(this)}>Try another word!</button></>
     } else {
       message = <><h2>Good try, but not quite right :(</h2>
       <p>{`The correct translation for ${this.state.preWord} was ${this.state.answer} and you chose ${this.state.guess_input}!`}</p></>;
       //if the user entered the wrong word
-      displayForm = <><button type='submit'>Try another word!</button></>
+      displayForm = <><button type='click' onClick={this.handleNext.bind(this)}>Try another word!</button></>
     } 
 
     //for if there is no answer
     if (!this.state.answer) {
       message = <><h2>Translate the word:</h2> <span>{this.state.nextWord}</span></>;
       displayForm = <><form onSubmit={this.handleSubmit.bind(this)}>
-        <label htmlFor='learn-guess-input'>What's the translation for this word? </label>
+        <label htmlFor='learn-guess-input'>What's the translation for this word?</label>
         <input type='text' id='learn-guess-input' name='guess_input' onChange={this.handleChange.bind(this)} required />
         <button type='submit'>Submit your answer</button>
       </form></>
